3cfb6c7625e47bd646c9a354935184ed2bd9c141
J-Drolet <79327056+J-Drolet@users.noreply.github.com>
2022-07-04 12:26:43 -0400

    Initalize Spring Project

---
+++ dogspring/src/main/java/io/github/jdrolet/dogspring/dogspring/DogspringApplication.java
@@ -0,0 +1,13 @@
+package io.github.jdrolet.dogspring.dogspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogspringApplication.class, args);
+	}
+
+}
---
+++ dogspring/src/test/java/io/github/jdrolet/dogspring/dogspring/DogspringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jdrolet.dogspring.dogspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
